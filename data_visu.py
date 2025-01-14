60ee38c2153f93f00ae34324cfd2c53e8d7f03ba
Pierre-Louis <pierre-louis.favreau@epita.fr>
2025-01-14 18:20:01 +0100

    fix: multiple images dataviz

--- data_visu.py
+++ data_visu.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import matplotlib.pyplot as plt
 
@@ -24,6 +26,19 @@ def display_images(images, titles, figsize=(15, 10)):
     plt.show()
 
 
+def display_multiple_images(image_paths, figsize=(15, 10)):
+    plt.figure(figsize=figsize)
+    for i, img_path in enumerate(image_paths):
+        img = load_image(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.subplot(2, 3, i + 1)
+        plt.imshow(img)
+        plt.title(f"Image {os.path.basename(img_path)}")
+        plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     path = "facades/train/"
     image_path = f"{path}91.jpg"
@@ -32,6 +47,10 @@ def main():
     image_real, image_cond = split_image(image)
 
     display_images([image_real, image_cond], ["Real", "Condition"], figsize=(18, 6))
+
+    image_names = ["91.jpg", "92.jpg", "93.jpg", "94.jpg", "95.jpg"]
+    image_paths = [f"{path}{img_name}" for img_name in image_names]
+    display_multiple_images(image_paths)
 
 
 if __name__ == "__main__":
